a9a583a1fc46be6d4475714497c66d54e6283fe7
filimon4uck <filimon4uck.yura@gmail.com>
2024-11-08 21:23:53 +0100

    created all components and screens

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
+import { NavigationContainer } from "@react-navigation/native";
 import * as SplashScreen from "expo-splash-screen";
-import { useFonts } from "expo-font";
 import React, { useEffect, useState } from "react";
+// import "react-native-gesture-handler";
+import { useFonts } from "expo-font";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import LoginScreen from "./screens/LoginScreen";
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import CreatePostsScreen from "./screens/CreatePostsScreen";
+import PostsScreen from "./screens/PostsScreen";
+import CommentsScreen from "./screens/CommentsScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+// import RegistrationScreen from "./screens/RegistrationScreen";
+
+// const MainStack = createStackNavigator();
 
 SplashScreen.preventAutoHideAsync();
 
@@ -9,6 +22,7 @@ const App = () => {
   const [fontsLoaded] = useFonts({
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
   });
 
   const [appIsReady, setAppIsReady] = useState(false);
@@ -28,7 +42,14 @@ const App = () => {
     return null;
   }
 
-  return <LoginScreen />;
+  return (
+    // <NavigationContainer>
+    //   <BottomTabNavigator />
+    // </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ProfileScreen />
+    </GestureHandlerRootView>
+  );
 };
 
 export default App;
